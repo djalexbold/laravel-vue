8f9eb5db4312281e987d7c0db9f6295389e04623
Alex <djalexbold@gmail.com>
2024-07-16 14:39:19 +0300

    start

---
+++ resources/js/Pages/About.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+const greeting = ref('Welcome to About page');
+</script>
+
+<template>
+    <v-btn
+        rounded
+        outlined
+        height="52"
+        class="mx-2"
+    />
+    <h1>{{ greeting }}</h1>
+    <Link href="/">Go to Welcome page</Link>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Link } from '@inertiajs/vue3';
+
+const greeting = ref('Welcome to Your Vue.js + TypeScript App');
+
+</script>
+
+<template>
+    <h1>{{ greeting }}</h1>
+    <Link href="/about">Go to About page</Link>
+</template>
